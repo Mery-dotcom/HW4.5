348bb84bea3343b454b0f630ee9ce64aab61f4e7
Mery-dotcom <meka-28list.ru>
2025-02-26 22:32:49 +0600

    Add changes in MessageResponse Add new Fragment

--- app/src/main/java/com/example/hw45/fragments/MessageFragment.kt
+++ app/src/main/java/com/example/hw45/fragments/MessageFragment.kt
@@ -3,7 +3,6 @@ package com.example.hw45.fragments
 import android.app.AlertDialog
 import androidx.fragment.app.viewModels
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
